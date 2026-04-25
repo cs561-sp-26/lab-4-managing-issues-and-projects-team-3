684186817f1fab121b2ae0e08cddffdbbecb2f29
Aj Protzel <ajprotzel@gmail.com>
2026-04-25 14:42:22 -0700

    Created roundModes.js close #23
    task: add roundModes.js
    
    Close #23

---
+++ scripts/roundsMode.js
@@ -0,0 +1,5 @@
+/*************************************************************************
+ * File: roundsMode.js
+ * This file contains functions that support the "Rounds" mode, 
+ * including the "Log Round" modal.
+*************************************************************************/
