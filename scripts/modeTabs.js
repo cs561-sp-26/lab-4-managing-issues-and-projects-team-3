8cc953976d0b8573976fd82eeb3509ba9398f09c
Dravid <supermegaawesomedude3460@yahoo.com>
2026-04-27 18:05:42 -0700

    Cleaned up modeTabs.js

--- scripts/modeTabs.js
+++ scripts/modeTabs.js
@@ -29,18 +29,10 @@
     //Switch tab panel
     GlobalModeTabPanels[GlobalCurrentMode.get()].classList.add("hidden");
     GlobalModeTabPanels[newMode].classList.remove("hidden");
+    //Switch app title
+    document.title = "SpeedScore: " + GlobalModeNames[newMode];
     GlobalCurrentMode.set(newMode); //Change mode
     GlobalFocusedMode.set(newMode); //Change focused mode
-    if (GlobalHistoryLogging) {
-        const historyObj = {
-            page: GlobalModeNumbersToModes.get(newMode),
-            mode: newMode,
-            path: GlobalModeNumbersToRoutes.get(newMode)  
-        };   
-        history.pushState(historyObj, "", historyObj.path);
-        console.log("Console: In switchMode; pushing state: ", 
-            JSON.stringify(historyObj));
-    }
 }
 
 /*************************************************************************  
